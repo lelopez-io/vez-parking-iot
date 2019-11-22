3481b927a208bda881a9be0fd4d53c8feea5dcda
lelopez <ux.lelopez@gmail.com>
2019-11-22 14:02:46 -0600

    proximity.js works and can update sensor data correctly while also logging it as needed

--- Tests/proximity.js
+++ Tests/proximity.js
@@ -1,23 +1,64 @@
-const five = require('johnny-five');
-const piIO = require('pi-io');
+let SPOT_NO_DATA = -1
+let SPOT_FREE = 0
+let SPOT_OCCUPIED = 1
+let SPOT_TRANSIENT = 2
 
-// GPIO18 --> P1-12
-// GPIO24 --> P1-18
+let sensorData = {
+  'Group_H': {
+    'spot_62': {
+      'status': SPOT_NO_DATA,
+      'cm_val': SPOT_NO_DATA
+    },
+    'spot_61': {
+      'status': SPOT_NO_DATA,
+      'cm_val': SPOT_NO_DATA
+    },
+    'spot_60': {
+      'status': SPOT_NO_DATA,
+      'cm_val': SPOT_NO_DATA
+    },
+    'spot_59': {
+      'status': SPOT_NO_DATA,
+      'cm_val': SPOT_NO_DATA
+    },
+    'spot_58': {
+      'status': SPOT_NO_DATA,
+      'cm_val': SPOT_NO_DATA
+    }
+  }
+}
 
-const board = new five.Board({
-  io: new piIO()
-});
 
-board.on('ready', () => {
-  const proximity = new five.Proximity({
-    controller: piIO.HCSR04,
-    triggerPin: 'P1-12',
-    echoPin: 'P1-18'
+
+if(process.env.HOST_ENV == 'EDGE') {
+  console.log('Setting up board for EDGE')
+  const five = require('johnny-five');
+  const piIO = require('pi-io');
+  const board = new five.Board({
+    io: new piIO()
   });
-
-
-  proximity.on("change", function() {
-    console.log("cm: ", this.cm);
+  
+  board.on('ready', () => {
+    const sensor1 = new five.Proximity({
+      controller: piIO.HCSR04,
+      triggerPin: 'P1-12',
+      echoPin: 'P1-18'
+    });
+  
+  
+    sensor1.on("data", function() {
+      sensorData.Group_H.spot_62.cm_val = this.cm
+      if(this.cm > 70 || this.cm == 0) {
+        sensorData.Group_H.spot_62.status = SPOT_FREE
+      } else {
+        sensorData.Group_H.spot_62.status = SPOT_OCCUPIED
+      }
+    });
+  
   });
+};
 
-});
+setInterval(() => {
+    console.log(sensorData)
+}, 2000);
+
